4737b51af05d4e1084eb9d9be5b62dbc766a19ab
f3wd <72391227+f3wd@users.noreply.github.com>
2020-12-01 12:38:48 +0300

    Add files via upload

--- connection.php
+++ connection.php
@@ -1,4 +1,4 @@
 <?php
-$con = mysqli_connect('localhost','root','') or die("unable to connect, please check your connection");
+$con = mysqli_connect('us-cdbr-east-02.cleardb.com	','b842908dda91cd','88bc124f') or die("unable to connect, please check your connection");
 mysqli_select_db($con,'userregistration');
 ?>
